223351545262ef5b7f8d98ddc32b8874b4b4277e
Kevin Towe <kevintowe27@gmail.com>
2021-08-15 08:50:49 -0500

    save

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import {  TwilioWebhookController } from './twilio-webhook.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, TwilioWebhookController],
   providers: [AppService],
 })
 export class AppModule {}
--- apps/server/src/app/app.service.ts
+++ apps/server/src/app/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { BehaviorSubject } from 'rxjs';
+
+export interface RegisteredUser {
+  number: string;
+  uid?: string;
+  name?: string;
+}
+
 
 @Injectable()
 export class AppService {
+
+  private registeredUsers = new BehaviorSubject<{[key: string]: RegisteredUser}>(null);
+  
   getData(): { message: string } {
     return { message: 'Welcome to server!' };
+  }
+
+  isRegisteredNumber(phoneNumber: string): boolean {
+    const keys = Object.keys(this.registeredUsers.value);
+    let isUser = false;
+
+    keys.forEach((key) => {
+      if(key === phoneNumber) isUser = true
+    })
+    
+    return isUser;
   }
 }
