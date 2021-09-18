56587071341ad2fee45ae71ce851c5465946b394
Kevin Towe <kevintowe27@gmail.com>
2021-09-18 14:51:53 -0500

    save

---
+++ libs/front-end/material/README.md
@@ -0,0 +1,7 @@
+# front-end-material
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test front-end-material` to execute the unit tests.
---
+++ libs/front-end/material/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'front-end-material',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/front-end/material',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
